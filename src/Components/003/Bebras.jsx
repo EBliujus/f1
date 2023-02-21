9422a97558f12d2a621b318f1f00cbb1bb276bf4
EBliujus <ev.bliujus@gmail.com>
2023-02-21 21:45:41 +0200

    1/2 react

--- src/Components/003/Bebras.jsx
+++ src/Components/003/Bebras.jsx
@@ -1,9 +1,26 @@
+import rand from "../../Functions/rand";
+
 function Bebras () {
+
+        const fun = () => {
+            let a = '';
+            for (let i = 1; i <= 9; i++) {
+                a = a + i
+            }
+            return a;
+        }
 
     return (
         <>
-            <h1><i>Bebras</i></h1>
-            <h2>Barsukas</h2>
+            <h1><i style={{
+                 color: "skyblue", 
+                 backgroundColor: rand(0, 1) ? 'coral' : 'pink'
+                 }}>Bebras</i></h1>
+            <h2 className="red">Barsuku yra 
+            {
+                fun()
+            }
+            </h2>
         </>
     );
 
