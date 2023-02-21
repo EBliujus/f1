594fac5568d12600f866ddfd3d0378bf5d431ade
EBliujus <ev.bliujus@gmail.com>
2023-02-21 21:06:01 +0200

    react

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import Bebras from './Components/003/Bebras';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <Bebras/>
+        <Bebras/>
+        <Bebras></Bebras>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
