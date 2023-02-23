b7a5a6303822d951ceb9f21e0960c61ffa53783f
EBliujus <ev.bliujus@gmail.com>
2023-02-23 15:55:40 +0200

    crud

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect, useState } from "react";
 import "./App.scss";
 import "./Components/007/Create"
 import Create from "./Components/007/Create";
+import { create } from "./Functions/localStorage";
 
 
-
+const KEY = 'wishList';
 
 
 
 function App() {
 
+  const [createData, setCreateData] = useState(null);
+  
+
+
+  useEffect(() => {
+    if (null === createData) {
+      return;
+    }
+    create(KEY, createData);
+  }, [createData]);
 
   return (
     <div className="container">
