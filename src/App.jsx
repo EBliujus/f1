0c14f6fac86379d7d09392cc42d9a9e817d7e321
EBliujus <ev.bliujus@gmail.com>
2023-02-28 15:44:26 +0200

    list sort

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,55 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import { useEffect, useState } from "react";
-import "./App.scss";
-import "./Components/007/Create"
-import Create from "./Components/007/Create";
-import Edit from "./Components/007/Edit";
-import List from "./Components/007/List";
-import { create, destroy, edit, read } from "./Functions/localStorage";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { useEffect, useState } from 'react';
+import './App.scss';
+import axios from 'axios';
 
 
-const KEY = 'wishList';
 
 function App() {
 
-  const [list, setList] = useState(null);
-  const [lastRefresh, setLastResfresh] = useState(Date.now());
-  const [createData, setCreateData] = useState(null);
-  const [deleteData, setDeleteData] = useState(null);
-  const [modalData, setModalData] = useState(null);
-  const [editData, setEditData] = useState(null);
+    const [users, setUsers] = useState(null);
 
-  useEffect(() => {
-    // Loading'o imitacija
-    // setTimeout(() => setList(read(KEY)), 1000);
-    // be imitacijos
-    setList(read(KEY));
-  }, [lastRefresh]);
-  
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/users')
+        .then(res => {
+            console.log(res.data);
+            setUsers(res.data.map((u, i) => ({...u, row: i})));
+        });
 
+    }, []);
 
-  useEffect(() => {
-    if (null === createData) {
-      return;
+    const sort = () => {
+        setUsers(u => [...u].sort((a, b) => a.name.localeCompare(b.name)));
     }
-    create(KEY, createData);
-    setLastResfresh(Date.now())
-  }, [createData]);
 
-  useEffect(() => {
-    if (null === deleteData) {
-      return;
+    const sortBack = () => {
+        setUsers(u => [...u].sort((a, b) => a.row - b.row));
     }
-    // tipo klientas(narsykle)
-    setList(l => l.filter(d => deleteData.id !== d.id))
-    // tipo serveris
-    destroy(KEY, deleteData.id);
-    setLastResfresh(Date.now())
-  }, [deleteData]);
 
-  useEffect(() => {
-    if (null === editData) {
-      return;
+    const nameLength = () => {
+        setUsers(u => [...u].sort((a, b) => b.name.length -  a.name.length));
     }
-    edit(KEY, editData, editData.id);
-    setLastResfresh(Date.now())
-  }, [editData]);
+
 
   return (
-
-  <>
-  <div className="container">
-      <div className="row">
-          <div className="col-4">
-              <Create setCreateData={setCreateData} />
-          </div>
-      <div className="col-8">
-          <List list={list} setDeleteData={setDeleteData} setModalData ={setModalData}/>
-      </div>
-      </div>
-  </div>
-  <Edit modalData={modalData} setModalData={setModalData} setEditData={setEditData}/>
-  </>
-    
+    <div className="App">
+        <header className="App-header">
+            <h1>Users List</h1>
+           <ul>
+            {
+                users?.map(u => <li key={u.id}>{u.name}<small style={{ fontSize: "50%"}}>{u.company.catchPhrase}</small></li>)
+/*              users
+                ? 
+                users.map(u => <li key={u.id}>{u.name}</li>)
+                :
+                <li>LOADING...</li> */
+            }
+           </ul>
+           <button className='btn btn-outline-primary m-4' onClick={sort}>Sort</button>
+           <button className='btn btn-outline-primary m-4' onClick={sortBack}>Sort Back</button>
+           <button className='btn btn-outline-primary m-4' onClick={nameLength}>Name Length</button> 
+      </header>
+    </div>
   );
 }
 
